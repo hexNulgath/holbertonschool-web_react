a26963f3ad8a4007733562fa360465c8fafb1dd7
hexNulgath <machadoignacio@hotmail.com>
2025-02-02 16:53:47 -0300

    created task 4

---
+++ TypeScript/task_4/js/subjects/Subjects.ts
@@ -0,0 +1,14 @@
+namespace Subjects {
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
+    }
+
+    export class Subject {
+        protected teacher: Teacher;
+
+        setTeacher(teacher: Teacher): void {
+            this.teacher = teacher;
+        }
+    }
+}
